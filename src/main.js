99371f62f27259ebd82a0738b03f3b18f3ce4ad3
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-08-21 22:36:03 +0200

    installed bootstrap

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main(){
+    const [temperature, setTemperature] = useState(null);
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    
+    return(
+        <div className="main-page">
+
+        </div>
+    )
+}
