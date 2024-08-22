c420c18c7b709278c8baafbae63508fa0eec3c5e
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-08-22 12:19:16 +0200

    hard coded weather content

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,6 +1,7 @@
 
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
 
 
 function Main(){
@@ -8,25 +9,34 @@ function Main(){
     const [weatherData, setWeatherData] = useState({ ready: false });
 
     return(
-        <div className="main-page">
-            <div className="container text-center">
+        <div className="main-page mx-auto">
+          <h1>Weather App</h1>
+            <div className="container">
   <div className="row">
     <div className="col">
       1 of 2
+      <h2 id="city">London</h2>
+      <img src=""/>
+      <div id="weather-temperature"></div>
+      <div id="date-time">Monday, 09:00</div>
+      <div id="description">Sunny</div>
     </div>
     <div className="col">
       2 of 2
     </div>
   </div>
   <div className="row">
-    <div class="col">
+    <div className="col">
       1 of 3
+      <div id="humidity">Humidity</div>
     </div>
     <div className="col">
+      <div id="visibility">Visibility</div>
       2 of 3
     </div>
     <div className="col">
       3 of 3
+      <div id="air-quality">Air Quality</div>
     </div>
   </div>
 </div>
